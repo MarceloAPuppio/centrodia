34a64925d57b0c86ca9f76cbcb0abe2afd6f5f8c
Marcelo Puppio <77312533+MarceloAPuppio@users.noreply.github.com>
2021-10-18 08:29:34 -0300

    fixed URL

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 import slider from "./slider.js";
 
 let arregloFotos = [
-  "url('../images/carrousel/1.webp')",
-  "url('../images/carrousel/2.webp')",
-  "url('../images/carrousel/3.webp')",
-  "url('../images/carrousel/4.webp')",
+  "url('images/carrousel/1.webp')",
+  "url('images/carrousel/2.webp')",
+  "url('images/carrousel/3.webp')",
+  "url('images/carrousel/4.webp')",
 ];
 const galery = document.querySelector(".galery");
 let clousure = slider(5000, galery, arregloFotos);
